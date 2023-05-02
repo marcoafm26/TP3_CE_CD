f05c81b2343cd22a9ddf0a43e7581c27b6afb9df
Marco Aurélio <marcoaureliofm26@gmail.com>
2023-05-01 21:09:08 -0300

    versao com switch para funcoes

--- src/app/main.java
+++ src/app/main.java
@@ -20,33 +20,43 @@ public class main {
     final double X_BOUND = 10 ;// 20 e -20
     final double F = 0.5;
     final double Cr = 0.8;
-    final int FUNCTION = 1;
+    final int FUNCTION = 2;
+    int QTD_GENES = 0;
+    int QTD_AVALIACAO = 0;
 
     switch (FUNCTION){
       case 1:
-        int QTD_GENES = 1;
-        int QTD_AVALIACAO
-
+         QTD_GENES = 1;
+         QTD_AVALIACAO = 2;
+        break;
+      case 2:
+         QTD_GENES = 2;
+         QTD_AVALIACAO = 2;
+        break;
+      case 3:
+         QTD_GENES = 3;
+         QTD_AVALIACAO = 3;
+        break;
     }
     FNDS fnds = new FNDS();
     ArrayList<Individuo> individuos = new ArrayList<>(NUM_INDIVIDUOS);
-    individuos.addAll(init(NUM_INDIVIDUOS,X_BOUND));
+    individuos.addAll(init(NUM_INDIVIDUOS,X_BOUND,QTD_GENES,QTD_AVALIACAO));
     // funcao = x1^2 + x2^2
 
 
-    Individuo exp = new Individuo();
+    Individuo exp = new Individuo(QTD_GENES,QTD_AVALIACAO);
     Random random = new Random();
 
-    avaliaIndividuos(individuos);
+    avaliaIndividuos(individuos,FUNCTION);
 
-    int INICIAL_GEN = 1;
+    int NUM_GEN = 1;
     ArrayList<Individuo> new_pop = null;
     ArrayList<Individuo> intermed_Pop = null;
 
     final List<Integer> genToPrint = Arrays.asList(1,20, 40, 60, 80,100, MAX_GEN);
 
 
-    while(INICIAL_GEN <= MAX_GEN){
+    while(NUM_GEN <= MAX_GEN){
 
       new_pop = new ArrayList<>(NUM_INDIVIDUOS);
       intermed_Pop = new ArrayList<>(NUM_INDIVIDUOS);
@@ -58,12 +68,12 @@ public class main {
         int r2 = random.nextInt(0,NUM_INDIVIDUOS-1);
         int r3 = random.nextInt(0,NUM_INDIVIDUOS-1);
 
-        Individuo u = new Individuo();
+        Individuo u = new Individuo(QTD_GENES,QTD_AVALIACAO);
         u.gerarIndividuo(individuos,r1,r2,r3,F);
 
 
-        exp = recombinar(individuos.get(i),u,Cr);
-        exp.avaliar();
+        exp = recombinar(individuos.get(i),u,Cr,QTD_GENES,QTD_AVALIACAO);
+        exp.avaliar(FUNCTION);
 
         intermed_Pop.add(exp);
       }
@@ -87,34 +97,29 @@ public class main {
 
       individuos = new_pop;
 
-      if (genToPrint.contains(INICIAL_GEN)) {
-        generatePonts(individuos, INICIAL_GEN);
+      if (genToPrint.contains(NUM_GEN)) {
+        generatePonts(individuos, NUM_GEN);
       }
 
-      INICIAL_GEN++;
+      NUM_GEN++;
     }
 
-    for (Individuo individuo :individuos) {
-      System.out.println();
+    for (int i = 0; i < individuos.size(); i++) {
+      String dadosDoIndividuo = "models.Individuo " + (i+1);
+      for (int j = 0; j < individuos.get(i).genes.length ; j++) {
+        dadosDoIndividuo += "\t Genes " + (j+1)+": " +  individuos.get(i).genes[j];
+      }
+      for (int j = 0; j <individuos.get(i).avaliacao.length ; j++) {
+        dadosDoIndividuo += "\t Avaliacao " + (j+1)+": " +  individuos.get(i).avaliacao[j];
+      }
+      System.out.println(dadosDoIndividuo);
     }
-
-    new_pop = new_pop;
-//    for (int i = 0; i < individuos.size(); i++) {
-//      String dadosDoIndividuo = "models.Individuo " + (i+1);
-//      for (int j = 0; j < individuos.get(i).genes.length ; j++) {
-//        dadosDoIndividuo += "\t Genes " + (j+1)+": " +  individuos.get(i).genes[j];
-//      }
-//      for (int j = 0; j <individuos.get(i).avaliacao.length ; j++) {
-//        dadosDoIndividuo += "\t Avaliacao " + (j+1)+": " +  individuos.get(i).avaliacao[j];
-//      }
-//      System.out.println(dadosDoIndividuo);
-//    }
   }
 
 
 
-  public static Individuo recombinar(Individuo individuo, Individuo u, double Cr){
-    Individuo filho = new Individuo();
+  public static Individuo recombinar(Individuo individuo, Individuo u, double Cr, int QTD_GENES,int QTD_AVALIACAO){
+    Individuo filho = new Individuo(QTD_GENES,QTD_AVALIACAO);
     for(int i = 0; i < individuo.genes.length ; i++){
       double r = new Random().nextDouble(0,1);
       if(r < Cr){
@@ -126,18 +131,17 @@ public class main {
     return filho;
   }
 
-  public  static void avaliaIndividuos(ArrayList<Individuo> individuos){
+  public  static void avaliaIndividuos(ArrayList<Individuo> individuos,int FUNCTION){
     for (Individuo individuo :
             individuos) {
-      individuo.avaliar();
+      individuo.avaliar(FUNCTION);
     }
-    individuos = individuos;
   }
 
-  public static ArrayList<Individuo> init(int NUM_INDIVIDUOS, double X_BOUND){
+  public static ArrayList<Individuo> init(int NUM_INDIVIDUOS, double X_BOUND,int QTD_GENES,int QTD_AVALIACAO){
     ArrayList<Individuo> popInicial = new ArrayList<>(NUM_INDIVIDUOS);
     for (int i = 0; i < NUM_INDIVIDUOS; i++) {
-      Individuo individuo = new Individuo();
+      Individuo individuo = new Individuo(QTD_GENES,QTD_AVALIACAO);
       individuo.gerarGenes(-X_BOUND,X_BOUND);
       popInicial.add(individuo);
     }
